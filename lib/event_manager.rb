cb5072323e517706723b508abfbaee09d49156f5
Chris Smith <42@chrisryansmith.com>
2017-04-06 15:00:49 -0600

    add erb template

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,21 +1,32 @@
 require 'pry'
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 require './lib/sanitize_module'
 
 class EventManager
   include Sanitize
+  Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
   def read_csv
     CSV.open "./data/event_attendees.csv", headers:true, header_converters: :symbol
   end
 
+  def retrieve_legislators_by(zip)
+    Sunlight::Congress::Legislator.by_zipcode(zip)
+  end
+
   def output
     contents = read_csv
+    template_letter = File.read "./lib/form_letter.erb"
+    erb_template = ERB.new template_letter
     contents.each do |row|
       name = row[:first_name]
       zipcode = clean_zipcode(row[:zipcode])
-      puts "#{name} #{zipcode}"
+      legislators = retrieve_legislators_by(zipcode)
+      form_letter = erb_template.result(binding)
+      puts form_letter
     end
   end
 end
+EventManager.new.output
--- lib/sanitize_module.rb
+++ lib/sanitize_module.rb
@@ -2,4 +2,11 @@ module Sanitize
   def clean_zipcode(zip)
     zip.to_s.rjust(5,"0")[0..4]
   end
+
+  def format_legislator_names(legislators)
+    legislator_names = legislators.collect do |legislator|
+      "#{legislator.first_name} #{legislator.last_name}"
+    end
+    legislator_names.join(", ")
+  end
 end
