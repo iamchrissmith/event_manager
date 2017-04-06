83f645b6262a026d9afef3567305c8b4978d4e59
Chris Smith <42@chrisryansmith.com>
2017-04-06 14:26:22 -0600

    display names by reading csv file

---
+++ lib/event_manager.rb
@@ -0,0 +1,10 @@
+require 'pry'
+require 'csv'
+
+contents = CSV.open "./data/event_attendees.csv",
+           headers:true,
+           header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  puts name
+end
