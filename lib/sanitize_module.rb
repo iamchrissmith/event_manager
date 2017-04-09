749584191c6a2011b97de25656a5f96663f2952e
Chris Smith <42@chrisryansmith.com>
2017-04-09 14:48:54 -0600

    clean phone number

--- lib/sanitize_module.rb
+++ lib/sanitize_module.rb
@@ -9,4 +9,25 @@ module Sanitize
     end
     legislator_names.join(", ")
   end
+
+  def clean_phone_number(number)
+    cleaned = remove_non_numerics(number.to_s)
+    return phone_format(cleaned) if cleaned.length == 10
+    # return '' if cleaned.length > 11 || cleaned.length < 10
+    if cleaned.length == 11 && cleaned[0] == "1"
+      return phone_format(cleaned[1..-1])
+    end
+    return ''
+  end
+
+  def remove_non_numerics(number)
+    number.gsub(/\D/, '')
+  end
+
+  def phone_format(number)
+    number.insert(0, "(")
+    number.insert(4, ") ")
+    number.insert(9, "-")
+    number
+  end
 end
--- spec/sanitize_spec.rb
+++ spec/sanitize_spec.rb
@@ -19,4 +19,31 @@ describe Sanitize do
     end
   end
 
+  describe ".clean_phone_number" do
+    it "does nothing when phone is ok" do
+      expect(subject.clean_phone_number("123-456-7890")).to eq "(123) 456-7890"
+      expect(subject.clean_phone_number("123.456.7890")).to eq "(123) 456-7890"
+      expect(subject.clean_phone_number("123 456 7890")).to eq "(123) 456-7890"
+      expect(subject.clean_phone_number("(123)456-7890")).to eq "(123) 456-7890"
+      expect(subject.clean_phone_number("(123) 456-7890")).to eq "(123) 456-7890"
+      expect(subject.clean_phone_number("1234567890")).to eq "(123) 456-7890"
+    end
+    it "returns '' if less than 10 digits" do
+      expect(subject.clean_phone_number("123-456-789")).to eq ""
+      expect(subject.clean_phone_number(nil)).to eq ""
+    end
+    it "trims first '1' if 11 digits" do
+      expect(subject.clean_phone_number("1 (123) 456-7890")).to eq "(123) 456-7890"
+      expect(subject.clean_phone_number("1-123-456-7890")).to eq "(123) 456-7890"
+      expect(subject.clean_phone_number("11234567890")).to eq "(123) 456-7890"
+    end
+    it "returns '' if 11 digits doesn't start with 1" do
+      expect(subject.clean_phone_number("2-123-456-7890")).to eq ''
+    end
+    it "returns '' if more than 11" do
+      expect(subject.clean_phone_number("112345678903")).to eq ""
+      expect(subject.clean_phone_number("112345678901")).to eq ""
+    end
+  end
+
 end
